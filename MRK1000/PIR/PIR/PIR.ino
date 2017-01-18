f078fdd41d9403dbed161863284bae94d07721f7
anphonic <abilly@gmail.com>
2017-01-18 12:30:11 -0600

    MRK1000 projects

---
+++ MRK1000/PIR/PIR/PIR.ino
@@ -0,0 +1,29 @@
+int ledPin = 7;
+int inputPin = 2;
+int pirState = LOW;
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin, OUTPUT);
+pinMode(inputPin,INPUT);
+
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(inputPin);
+    if (val==HIGH){
+      digitalWrite(ledPin,HIGH);
+      if(pirState == LOW){
+        Serial.println("motion");
+        pirState = HIGH;
+      }
+    } else {
+      digitalWrite(ledPin, LOW);
+      Serial.println("no motion");
+      pirState = LOW;
+      }
+}
+
---
+++ MRK1000/WifiNoEncryption_GY-61/WifiNoEncryption_GY-61/WifiNoEncryption_GY-61.ino
@@ -0,0 +1,157 @@
+/*
+
+ This example connects to an unencrypted WiFi network.
+ Then it prints the  MAC address of the WiFi shield,
+ the IP address obtained, and other network details.
+
+ Circuit:
+ * WiFi shield attached
+
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 31 May 2012
+ by Tom Igoe
+ */
+#include <SPI.h>
+#include <WiFi101.h>
+
+char ssid[] = "AM-guest2-4";     // the name of your network
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
+const int xpin = A0;                  // x-axis of the accelerometer
+const int ypin = A1;                  // y-axis
+const int zpin = A2;                  // z-axis (only on 3-axis models)
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to open SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWiFiData();
+}
+
+void loop() {
+  // check the network connection once every 10 seconds:
+  delay(10000);
+  printCurrentNet();
+  checkAcel();
+}
+
+void printWiFiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+
+  // print your subnet mask:
+  IPAddress subnet = WiFi.subnetMask();
+  Serial.print("NetMask: ");
+  Serial.println(subnet);
+
+  // print your gateway address:
+  IPAddress gateway = WiFi.gatewayIP();
+  Serial.print("Gateway: ");
+  Serial.println(gateway);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5], HEX);
+  Serial.print(":");
+  Serial.print(bssid[4], HEX);
+  Serial.print(":");
+  Serial.print(bssid[3], HEX);
+  Serial.print(":");
+  Serial.print(bssid[2], HEX);
+  Serial.print(":");
+  Serial.print(bssid[1], HEX);
+  Serial.print(":");
+  Serial.println(bssid[0], HEX);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+}
+
+void checkAcel() {
+  int x = analogRead(xpin);  //read from xpin
+ 
+ int y = analogRead(ypin);  //read from ypin
+ 
+ int z = analogRead(zpin);  //read from zpin
+ 
+float zero_G = 512.0; //ADC is 0~1023  the zero g output equal to Vs/2
+                      //ADXL335 power supply by Vs 3.3V
+float scale = 102.3;  //ADXL335330 Sensitivity is 330mv/g
+                       //330 * 1024/3.3/1000  
+
+/*
+Serial.print(((float)x - 331.5)/65*9.8);  //print x value on serial monitor
+Serial.print("-X\t");
+Serial.print(((float)y - 329.5)/68.5*9.8);  //print y value on serial monitor
+Serial.print("-Y\t");
+Serial.print(((float)z - 340)/68*9.8);  //print z value on serial monitor
+Serial.print("-Z\n");
+*/
+Serial.print(x); 
+Serial.print("-X\t");
+Serial.print(y);
+Serial.print("-Y\t");
+Serial.print(z);  
+Serial.print("-Z\n");
+delay(500);
+}
+
+
